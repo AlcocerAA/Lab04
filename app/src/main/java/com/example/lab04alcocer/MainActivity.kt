90315f9201b5767a22cb4c74bbbcceb246fc723b
alcocer <alexanderalcocer21092728@gmail.com>
2025-09-12 20:04:54 -0500

    feat: cambio desde rama-conflicto

--- app/src/main/java/com/example/lab04alcocer/MainActivity.kt
+++ app/src/main/java/com/example/lab04alcocer/MainActivity.kt
@@ -1,33 +1,27 @@
-package com.example.lab04alcocer
+package com.example.myapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.lab04alcocer.ui.theme.Lab04AlcocerTheme
+import com.example.myapp.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Lab04AlcocerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Greeting("Android")   // Primer componente
-                        SecondComponent()    // Segundo componente
-                    }
+            MyAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("TECSUP 👋") // ← aquí cambié el texto
                 }
             }
         }
@@ -35,32 +29,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "¡Hola, $name!",
-        color = Color(0xFF6200EE),
-        fontSize = 26.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = modifier
-    )
-}
-
-@Composable
-fun SecondComponent() {
-    Text(
-        text = "Este es el segundo componente con nuevo estilo",
-        color = Color.Gray,
-        fontSize = 18.sp
-    )
+fun Greeting(name: String) {
+    Text(text = "Hola $name, bienvenido a mi primera app!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Lab04AlcocerTheme {
-        Column {
-            Greeting("Android")
-            SecondComponent()
-        }
+    MyAppTheme {
+        Greeting("Preview")
     }
 }
